eb6b9275e2cd295a399287f55a51ab04d35b3439
renkarsu <42439654+renkarsu@users.noreply.github.com>
2019-12-27 19:23:33 +0900

    complete move
    not juming

---
+++ Assets/Scripts/playerController2.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController2 : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed = 2.0f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //scene内の赤い矢印(x軸)が向いてる方向をtransform.rightは示す.
+            //objectの向いてる向きではないので,objectをsceneの座標と別の向きに配置すると
+            //右にいってほしくてもゲーム制作者から見て前に向かったりする.
+            rb.velocity = transform.right * speed;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = -transform.right * speed;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+                                               //x, y,  z
+            rb.velocity = new Vector3(0, 0, 1) * speed;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.velocity = new Vector3(0, 0, -1) * speed;
+        }
+    }
+
+}
---
+++ Assets/Scripts/playerController3.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController3 : MonoBehaviour
+{
+    public float movementSpeed = 5.0f;
+    public float rotationSpeed = 200.0f;
+    void Update()
+    {
+        //tips:プレイヤの向きはマウスで変えて横への移動をwasdでしたければ
+        //transform.???(?, ?, ?) * Time.deltaTime * movementSpeed);
+        //これを14行目のtransform.Rotateを消して？を埋めて上の記述を書くと
+        //A,Dキーまたは右,左矢印はプレイヤの向きを変えるのではなく,横に移動するコマンドになる.
+        transform.Rotate(0, Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed, 0);
+        transform.Translate(0, 0, Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed);
+    }
+
+}
